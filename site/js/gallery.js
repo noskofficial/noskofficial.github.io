21067f02438b1b8b6fdaca7105471971f056cd34
NameLesssNerd <itsmesushant20xx@gmail.com>
2023-05-10 18:06:13 +0545

    navbar is made Responsive and GalleryImage view is made advanced where users can see next and previous picture

--- site/js/gallery.js
+++ site/js/gallery.js
@@ -2,31 +2,40 @@
 let images = Array.from(document.getElementsByClassName("view-image"));
 let gallery = document.getElementById("gallery");
 var modal = document.getElementById('myModal');
-
-
+var modalImg = document.getElementById("modal-img");
+let userClickedImagePosition;
 const close_modal = () => {
     gallery.style.display = "block";
     modal.style.display = "none";
 }
-
-
-const view_image = (e) => {
-    gallery.style.display = "none";
- 
-    var modalImg = document.getElementById("modal-img");
-    modal.style.display = "block";
-    modalImg.src = e.target.src;
-    modalImg.alt = e.target.alt;
-    modalImg.style.width = e.target.naturalWidth;
-    modalImg.style.height = e.target.naturalHeight;
-
-    var close_button = document.getElementById("close-btn")
-    close_button.addEventListener("click", close_modal);
+const managePrevnNext = (position) => {
+    userClickedImagePosition = position
 }
-
-images.forEach(image => {
-    image.addEventListener("click", view_image);
+const view_image = ({ src, alt }) => {
+    gallery.style.display = "none";
+    modal.style.display = "block";
+    modalImg.src = src;
+    modalImg.alt = alt;
+    var close_button = document.getElementById("close-btn")
+    const previousButton = document.getElementById("previousButton")
+    const nextButton = document.getElementById("nextButton")
+    close_button.addEventListener("click", close_modal);
+    previousButton.addEventListener('click', () => {
+        if ((userClickedImagePosition == 0) || (userClickedImagePosition > 0)) {
+            userClickedImagePosition == 0 ? "" : userClickedImagePosition--
+            modalImg.src = images[userClickedImagePosition].currentSrc;
+        }
+    })
+    nextButton.addEventListener('click', () => {
+        if ((userClickedImagePosition == (images.length - 2)) || (userClickedImagePosition < images.length)) {
+            userClickedImagePosition == (images.length - 2) ? "" : userClickedImagePosition++
+            modalImg.src = images[userClickedImagePosition].currentSrc;
+        }
+    })
+}
+images.forEach((image, index) => {
+    image.addEventListener("click", () => {
+        view_image(image)
+        managePrevnNext(index)
+    });
 });
-
-
-
